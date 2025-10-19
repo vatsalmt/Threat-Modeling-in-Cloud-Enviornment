b4a028d32418ca1dff9f985ded413882bf382dd7
Parth Kadiya <91082703+ParthKadiya@users.noreply.github.com>
2025-10-19 15:54:40 -0500

    Add files via upload

---
+++ iac_samples/open_sg.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "bad_sg" {
+  name        = "open-sg"
+  description = "Security group with open SSH access"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
