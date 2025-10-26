51f80ddf515e82b8bfb70419de9bc9f87adc7915
Parth Kadiya <91082703+ParthKadiya@users.noreply.github.com>
2025-10-26 14:36:17 -0500

    Delete iac_samples directory

--- iac_samples/cf_s3_public.yaml
+++
@@ -1,5 +0,0 @@
-Resources:
-  PublicBucket:
-    Type: AWS::S3::Bucket
-    Properties:
-      AccessControl: PublicRead
--- iac_samples/s3_public.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bad_bucket" {
-  bucket = "public-bucket-example"
-  acl    = "public-read"
-}
