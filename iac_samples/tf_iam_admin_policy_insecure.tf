c301bef736e9997aee2bea6fe8e8cb21f3fa8034
Parth Kadiya <91082703+ParthKadiya@users.noreply.github.com>
2025-10-26 14:57:21 -0500

    Update tf_iam_admin_policy_insecure.tf

--- iac_samples/tf_iam_admin_policy_insecure.tf
+++ iac_samples/tf_iam_admin_policy_insecure.tf
@@ -1,5 +1,3 @@
-# Insecure: Wildcard IAM policy (Admin)
-# Hints: STRIDE=Elevation of Privilege; ATT&CK=T1078 (Valid Accounts), T1098 (Account Manipulation)
 provider "aws" { region = "us-east-1" }
 
 data "aws_iam_policy_document" "admin" {
